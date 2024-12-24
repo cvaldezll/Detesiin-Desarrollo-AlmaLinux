11019325b28eddfae942f4bdf04783b0a5d8800d
Christian Valdez - Pc <cvaldez@detesiin.com>
2024-12-23 20:54:05 -0500

    actualicé el archivo q será respaldo.sh

--- confbak_respaldo.sh
+++ confbak_respaldo.sh
@@ -4,7 +4,9 @@ echo "Borrando los backups previos..."
 rm /home/oficina/Respaldo/Desarrollo.zip
 rm /home/oficina/Respaldo/Produccion.zip
 rm /home/oficina/Respaldo/Documentos.zip
-#
+
+echo ""
+
 echo "Generando los nuevos backups..."
 zip -r /home/oficina/Respaldo/Desarrollo /home/oficina/Desarrollo
 zip -r /home/oficina/Respaldo/Produccion /home/oficina/Produccion
